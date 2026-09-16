33be00f6dfd2da4cad63f9498941a36ad7854513
zeplay05 <music132548@gmail.com>
2026-09-16 20:09:55 +0700

    fix: set proper outputDirectory test project/dist for Vercel

--- test project/vite.config.js
+++ test project/vite.config.js
@@ -4,10 +4,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
   server: {
     proxy: {
       '/api/onesignal': {
